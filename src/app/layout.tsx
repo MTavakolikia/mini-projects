e5de69aa8e8caf1d58a9acad320379785fb13a40
mohammad <mohammadtavakolikia66@gmail.com>
2025-08-29 20:34:30 +0330

    feat(layout) : add font family base on language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/Header";
 import { Box, HStack } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 
 
 export default function AppLayout() {
+    const { i18n } = useTranslation();
+    const fontFamily = i18n.language === 'fa' ? "'Vazir', sans-serif" : "'Roboto', sans-serif";
     return (
-        <Box as={"section"} h={"dvh"} bg={"gray.100"} _dark={{ bg: "gray.900" }}>
+        <Box as={"section"} h={"dvh"} bg={"gray.100"} _dark={{ bg: "gray.900" }}   fontFamily={fontFamily} >
             <Header />
             <HStack as="main" p={4} >
                 <Outlet />
